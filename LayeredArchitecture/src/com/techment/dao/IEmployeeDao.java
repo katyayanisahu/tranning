8f726593517b5e9678dbe70d5793c011ae8d4ad0
katyayanisahu <88482844+katyayanisahu@users.noreply.github.com>
2021-08-15 21:17:27 +0700

    Delete IEmployeeDao.java

--- LayeredArchitecture/src/com/techment/dao/IEmployeeDao.java
+++
@@ -1,8 +0,0 @@
-package com.techment.dao;
-import java.util.List;
-import com.techment.modal.Employee;
-public interface IEmployeeDao {
-  Employee getEmployeeById(int id);
-  List<Employee> getEmployeeByDept(String dept);
-  
-} 
