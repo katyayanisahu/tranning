1e0244f165191a5a24bfadf9cb6cc846ec401251
katyayanisahu <88482844+katyayanisahu@users.noreply.github.com>
2021-09-07 00:40:23 +0530

    Add files via upload

---
+++ Springbootprog-first-app/src/main/java/com/springboot/first/app/HelloWorldController.java
@@ -0,0 +1,20 @@
+package com.springboot.first.app;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+public class HelloWorldController {
+	
+	//GET HTTP Method
+	//http://localhost:8000//hello-world
+	@GetMapping("/hello-world")
+	public String helloWorld()
+	{
+		return "Hello World!";
+	}
+
+
+}
